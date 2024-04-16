06f14d565e7058856fafbf3f01b6f79db5f2b124
Soner Sayakci <s.sayakci@shopware.com>
2024-04-16 14:08:39 +0200

    feat: make command output more verbose

--- src/Helper/ProcessHelper.php
+++ src/Helper/ProcessHelper.php
@@ -21,8 +21,16 @@ class ProcessHelper
      */
     public function run(array $args): void
     {
+        $completeCmd = ['php', ...$args];
+
         $process = new PhpSubprocess($args, $this->projectDir);
-        $process->setPty(true);
+
+        $startTime = $this->printPreStart($completeCmd);
+
+        if (function_exists('stream_isatty') && stream_isatty(\STDOUT)) {
+            $process->setTty(true);
+        }
+
         $process->run(function (string $type, string $buffer): void {
             if ($type === Process::ERR) {
                 fwrite(\STDERR, $buffer);
@@ -34,6 +42,8 @@ class ProcessHelper
         if (!$process->isSuccessful()) {
             throw new \RuntimeException('Execution of ' . implode(' ', $args) . ' failed');
         }
+
+        $this->printPostStart($completeCmd, $startTime);
     }
 
     /**
@@ -46,7 +56,14 @@ class ProcessHelper
 
     public function runAndTail(string $code): void
     {
+        $start = $this->printPreStart([$code]);
+
         $process = new Process(['sh', '-c', $code], $this->projectDir);
+
+        if (function_exists('stream_isatty') && stream_isatty(\STDOUT)) {
+            $process->setTty(true);
+        }
+
         $process->run(function (string $type, string $buffer): void {
             if ($type === Process::ERR) {
                 fwrite(\STDERR, $buffer);
@@ -58,10 +75,48 @@ class ProcessHelper
         if (!$process->isSuccessful()) {
             throw new \RuntimeException('Execution of ' . $code . ' failed');
         }
+
+        $this->printPostStart([$code], $start);
     }
 
     public function getPluginList(): string
     {
         return (new PhpSubprocess(['bin/console', 'plugin:list', '--json'], $this->projectDir))->mustRun()->getOutput();
     }
+
+    /**
+     * @param array<string> $cmd
+     */
+    private function printPreStart(array $cmd): float
+    {
+        $cmdString = implode(' ', $cmd);
+        $startTime = microtime(true);
+
+        fwrite(\STDOUT, PHP_EOL);
+        fwrite(\STDOUT, "=================================================\n");
+        fwrite(\STDOUT, sprintf("Start: %s\n", $cmdString));
+        fwrite(\STDOUT, "=================================================\n");
+        fwrite(\STDOUT, PHP_EOL);
+
+        return $startTime;
+    }
+
+    /**
+     * @param array<string> $cmd
+     */
+    private function printPostStart(array $cmd, float $startTime): void
+    {
+        fwrite(\STDOUT, "=================================================\n");
+        fwrite(\STDOUT, sprintf("End: %s\n", implode(' ', $cmd)));
+        fwrite(\STDOUT, sprintf(
+            "> Time: %sms\n",
+            number_format((microtime(true) - $startTime) * 1000, 2, '.', ''),
+        ));
+
+        fwrite(\STDOUT, "=================================================\n");
+        fwrite(\STDOUT, PHP_EOL);
+    }
+
+
+
 }
