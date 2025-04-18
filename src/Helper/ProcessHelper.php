d2fa528106749cfcd2632f847eafbb907b982070
Joshua Behrens <behrens@heptacom.de>
2025-04-18 18:21:55 +0200

    Add deployment helper hints to the shell output to understand reason of execution
    Solves #41

--- src/Helper/ProcessHelper.php
+++ src/Helper/ProcessHelper.php
@@ -146,6 +146,8 @@ class ProcessHelper
 
         fwrite(\STDOUT, \PHP_EOL);
         fwrite(\STDOUT, "=================================================\n");
+        fwrite(\STDOUT, "============== [deployment-helper] ==============\n");
+        fwrite(\STDOUT, "=================================================\n");
         fwrite(\STDOUT, \sprintf("Start: %s\n", $cmdString));
         fwrite(\STDOUT, "=================================================\n");
         fwrite(\STDOUT, \PHP_EOL);
@@ -158,6 +160,8 @@ class ProcessHelper
      */
     private function printPostStart(array $cmd, float $startTime): void
     {
+        fwrite(\STDOUT, "=================================================\n");
+        fwrite(\STDOUT, "============== [deployment-helper] ==============\n");
         fwrite(\STDOUT, "=================================================\n");
         fwrite(\STDOUT, \sprintf("End: %s\n", implode(' ', $cmd)));
         fwrite(\STDOUT, \sprintf(
